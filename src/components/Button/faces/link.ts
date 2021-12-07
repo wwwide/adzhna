0a69e0ec52dea732881055300e5fbbfcea709266
Andrey Barkanov <andrey.barkanov@gmail.com>
2021-12-07 23:56:17 +0300

    Update link button style

--- src/components/Button/faces/link.ts
+++ src/components/Button/faces/link.ts
@@ -1,32 +1,33 @@
-import SC from 'styled-components'
+import SC, { css } from 'styled-components'
 import { ButtonStyledProps } from '../ButtonStyledProps'
 import { Base } from './base'
 
-export const Link = SC(Base)<ButtonStyledProps>`
-  padding: 0 .5rem;
+export const Link = SC(Base)<ButtonStyledProps>`${css`
+  padding: 0 0.5rem;
   background: ${(p) => p.theme.adzhna.colors.link.fill.normal};
   color: ${(p) => p.theme.adzhna.colors.link.text.normal};
   box-shadow: none;
   border-radius: 0;
-  
+  text-transform: none;
+
   &:hover {
-    background: ${(p) => p.theme.adzhna.colors.link.fill.hover};;
+    background: ${(p) => p.theme.adzhna.colors.link.fill.hover};
     color: ${(p) => p.theme.adzhna.colors.link.text.hover};
     border-bottom: 1px dashed ${(p) => p.theme.adzhna.colors.link.text.hover};
   }
-  
+
   &:active {
-    background: ${(p) => p.theme.adzhna.colors.link.fill.active};;
+    background: ${(p) => p.theme.adzhna.colors.link.fill.active};
     color: ${(p) => p.theme.adzhna.colors.link.text.active};
     transform: scale(1);
   }
-  
+
   &:focus {
     border-radius: ${(p) => p.theme.adzhna.borders.radius}px;
   }
-  
+
   &:disabled {
     background: none !important;
     border-bottom: none;
   }
-`
+`}`
