2112f18ce5129f038f6799ff865a83e78721e6bd
Andrey <andrey.barkanov@gmail.com>
2022-01-05 14:00:34 +0300

    Export InputProps

--- src/components/Input/index.ts
+++ src/components/Input/index.ts
@@ -1,6 +1,6 @@
 export * from './getInputByKind'
 export * from './InputKind'
 export * from './InputTheme'
-export { CommonInput as Input } from './kinds/CommonInput'
+export { CommonInput as Input, CommonInputProps as InputProps } from './kinds/CommonInput'
 export * from './kinds/MaskedInput'
 export * from './kinds/PhoneInput'
