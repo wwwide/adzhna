6cbd47351f803acf953cc34b48fe134bcf369968
Andrey Barkanov <andrey.barkanov@gmail.com>
2022-08-30 11:36:25 +0300

    Fix dialof overflow hook #2

--- src/components/Dialog/hooks/useBehavior.ts
+++ src/components/Dialog/hooks/useBehavior.ts
@@ -4,31 +4,31 @@ export const useBehavior = (open: boolean, scrollableContainers: string[]) => {
   useEffect(() => {
     if (open) {
       document.body.setAttribute('data-overflow', document.body.style.overflow)
-      document.body.setAttribute('overflow', 'hidden')
+      document.body.style.overflow = 'hidden'
 
       scrollableContainers.forEach((container) => {
-        const item = document.querySelector(container)
+        const item = document.querySelector<HTMLElement>(container)
         if (item) {
-          item.setAttribute('data-overflow', item.getAttribute('overflow') || '')
-          item.setAttribute('overflow', 'hidden')
+          item.setAttribute('data-overflow', item.style.overflow)
+          item.style.overflow = 'hidden'
         }
       })
     } else {
       const old = document.body.getAttribute('data-overflow')
       if (old) {
-        document.body.setAttribute('overflow', old)
+        document.body.style.overflow = old
       } else {
-        document.body.removeAttribute('overflow')
+        document.body.style.overflow = ''
       }
 
       scrollableContainers.forEach((container) => {
-        const item = document.querySelector(container)
+        const item = document.querySelector<HTMLElement>(container)
         if (item) {
           const old = item.getAttribute('data-overflow')
           if (old) {
-            item.setAttribute('overflow', old)
+            item.style.overflow = old
           } else {
-            item.removeAttribute('overflow')
+            item.style.overflow = ''
           }
         }
       })
