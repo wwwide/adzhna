e8e9313445e2e351c61ddafa747f10d19c758877
Andrey Barkanov <andrey.barkanov@gmail.com>
2022-08-30 11:08:10 +0300

    Fix dialof overflow hook

--- src/components/Dialog/hooks/useBehavior.ts
+++ src/components/Dialog/hooks/useBehavior.ts
@@ -1,30 +1,37 @@
-import { useEffect, useState } from 'react'
-
-type SavedOverflows = {
-  [key: string]: string
-}
+import { useEffect } from 'react'
 
 export const useBehavior = (open: boolean, scrollableContainers: string[]) => {
-  const [overflows, setOverflows] = useState<SavedOverflows>({
-    body: document.body.style.overflow,
-  })
-
   useEffect(() => {
     if (open) {
-      const copy = { ...overflows }
-      copy.body = document.body.style.overflow
-      scrollableContainers.forEach((container) => {
-        copy[container] = document.querySelector(container)?.getAttribute('overflow') || ''
-      })
-      setOverflows(copy)
-    } else {
-      document.body.style.overflow = overflows.body
+      document.body.setAttribute('data-overflow', document.body.style.overflow)
+      document.body.setAttribute('overflow', 'hidden')
+
       scrollableContainers.forEach((container) => {
         const item = document.querySelector(container)
         if (item) {
-          item.setAttribute('overflow', overflows[container])
+          item.setAttribute('data-overflow', item.getAttribute('overflow') || '')
+          item.setAttribute('overflow', 'hidden')
+        }
+      })
+    } else {
+      const old = document.body.getAttribute('data-overflow')
+      if (old) {
+        document.body.setAttribute('overflow', old)
+      } else {
+        document.body.removeAttribute('overflow')
+      }
+
+      scrollableContainers.forEach((container) => {
+        const item = document.querySelector(container)
+        if (item) {
+          const old = item.getAttribute('data-overflow')
+          if (old) {
+            item.setAttribute('overflow', old)
+          } else {
+            item.removeAttribute('overflow')
+          }
         }
       })
     }
-  }, [open, overflows, setOverflows])
+  }, [open])
 }
